ebc407b4e6aef8a89f530c27251f37eb4063765d
Dante Lacuadra <55060638+dantelacu2@users.noreply.github.com>
2024-04-29 10:42:23 -0400

    Add rating system for previous matches

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -4,9 +4,26 @@ import Card from "@mui/material/Card";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
-import { Avatar, Button } from "@mui/material";
+import { Avatar, Button, Modal } from "@mui/material";
+import Rating from '@mui/material/Rating';
+
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  '& > legend': { mt: 2 },
+};
 
 function ProfileCard({ ...props }) {
+  const [rateModalOpen, setRateModalOpen] = React.useState(false);
+  const [ratings, setRatings] = React.useState({});
+
   return (
     <Card variant="outlined" sx={{ mx: 8 }}>
       <Box sx={{ p: 2 }}>
@@ -46,12 +63,52 @@ function ProfileCard({ ...props }) {
         </Button>
         <Button
           variant="contaned"
+          onClick={() => setRateModalOpen(true)}
           fullWith
           color="primary"
           sx={{ backgroundColor: "orange", color: "white", m: 1 }}
         >
           Rate Match
         </Button>
+        <Modal
+          open={rateModalOpen}
+          onClose={() => setRateModalOpen(false)}
+        >
+          <Box sx={modalStyle}>
+            <Typography id="rate-match-title" variant="h6" component="h2">
+              How good of a match was {props.match.name}?
+            </Typography>
+            <Typography component="legend">How likely would you live with this person?</Typography>
+            <Rating
+              name="simple-controlled"
+              value={ratings['live_with_rating']}
+              onChange={(event, newValue) => {
+                setRatings({...ratings, live_with_rating: newValue});
+              }}
+            />
+            <Typography component="legend">How well did this persons apartment must-haves align with yours?</Typography>
+              <Rating
+                name="simple-controlled"
+                value={ratings['must_haves']}
+                onChange={(event, newValue) => {
+                  setRatings({...ratings, must_haves: newValue});
+                }}
+            />
+            <Typography component="legend">How well did this persons roommate qualities align with yours?</Typography>
+              <Rating
+                name="simple-controlled"
+                value={ratings['roommate_qualities']}
+                onChange={(event, newValue) => {
+                  setRatings({...ratings, roommate_qualities: newValue});
+                }}
+            />
+          <Box style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button onClick={() => setRateModalOpen(false)} sx={{ backgroundColor: "orange", color: "white", m: 1 }}>
+              Save
+            </Button>
+          </Box>
+          </Box>
+        </Modal>
       </Box>
     </Card>
   );
